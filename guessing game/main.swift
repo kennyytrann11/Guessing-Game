7cba2885b5e7ae26961c900fd929f61470794c7e
Kenny Tran <kennytran@Interapts-MacBook-Pro-174.local>
2018-01-23 15:44:39 -0500

    first commit

---
+++ guessing game/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  guessing game
+//
+//  Created by Kenny Tran on 1/23/18.
+//  Copyright © 2018 Kenny Tran. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
